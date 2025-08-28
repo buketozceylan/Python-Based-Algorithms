1e68de5065d5ded3e29f1c360d5fbada435d4967
Buket Özceylan <buketozceylan@outlook.com>
2025-08-28 18:21:26 +0300

    Sixth exercise added

---
+++ Chapter 1 Basic/compare-two-numbers.py
@@ -0,0 +1,9 @@
+number1 = int(input('Enter your first number: '))
+number2 = int(input('Enter your second number: '))
+
+if number1 == number2:
+    print('Numbers are equal.')
+elif number1 > number2:
+    print('First number is bigger than second number.')
+else:
+     print('Second number is bigger than first number.')       
