4897ec6a2f1e32f8cd6d3165e21601677c14cada
Buket Özceylan <buketozceylan@outlook.com>
2025-08-28 18:52:12 +0300

    Ninth exercise added

---
+++ Chapter 1 Basic/coding-phone-number-in-text.py
@@ -0,0 +1,12 @@
+phone = input('Enter your phone number: ')
+
+# phoneList = list(str(phone))      #make list of the numbers one by one         
+
+numbers = ("Zero" , "One" , "Two" , "Three" , "Four" , "Five" , "Six" , "Seven" , "Eight" , "Nine")
+
+write = []
+
+for i in phone:
+    write.append(numbers[int(i)])
+
+print(write)
