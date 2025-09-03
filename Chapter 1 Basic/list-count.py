af127e791b77189df937355e01b953bc2dc1ea00
Buket Özceylan <buketozceylan@outlook.com>
2025-09-03 14:21:27 +0300

    Thirteenth exercise added

---
+++ Chapter 1 Basic/list-count.py
@@ -0,0 +1,9 @@
+data = [33,35,21,45,35,33,67,78,89,89,56,65,45,67,67,67,67,56,78,44,88]
+
+userInput = int(input('Enter the number you want to count: '))
+
+dataCount = data.count(userInput)
+
+
+
+print(f'Your number count: {dataCount}')
